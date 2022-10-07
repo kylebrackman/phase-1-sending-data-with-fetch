cadaffafc83d6c07045fdcf04e3f3069eca9e6a4
Kyle Brackman <kyle.a.brackman@gmail.com>
2022-10-06 19:58:17 -0600

    commit tree ae5adddb69566e8d1386c0f38488f06a1ee4b669

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Add your code here
+// practice
+function submitData() {
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then(res => res.json())
+    .then((data) => document.body.innerHTML = data.id)
+
+    .catch(error => document.body.innerHTML = error.message)
+}
+const userInfo = {
+    name: "Steve",
+    email: "steve@steve.com"
+};
+const configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify(userInfo),
+}
+
+
+
+
+// // // Add your code here
+// function submitData() {
+//     return fetch("http://localhost:3000/users", configurationObject)
+//     .then(res => res.json())
+//     .then((data) => document.body.innerHTML = data.id)
+
+//     .catch(error => document.body.innerHTML = error.message)
+// }
+// const userInfo = {
+//     name: "Steve",
+//     email: "steve@steve.com"
+// };
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json",
+//     },
+//     body: JSON.stringify(userInfo),
+// }
+
